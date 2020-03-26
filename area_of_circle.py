2a804ca8ce8367cb1ab2592ce4d1c6d55c572968
Oluwafemi <Icekidz1997@gmail.com>
2020-03-26 21:49:12 +0100

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,13 @@
+
+
+def area_of_circle(radius):
+    radius_of_circle= radius ** 2
+    area_of_circle= radius_of_circle * 3.142
+
+    print "the area of your circle is " + str(area_of_circle)
+
+
+
+area_of_circle(20)
+    
+    
